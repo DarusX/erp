82379245b0f3f15a0cbf1bf12da1b827b72fc907
Fabián Montero <mntr_rdrgz@hotmail.com>
2018-05-18 10:15:22 -0500

    ***

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,9 +1,5 @@
 <!DOCTYPE html>
-<!-- saved from url=(0054)https://keenthemes.com/preview/metronic/theme/admin_4/ -->
 <html lang="en">
-<!--<![endif]-->
-<!-- BEGIN HEAD -->
-
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 
@@ -12,27 +8,19 @@
     <meta content="width=device-width, initial-scale=1" name="viewport">
     <meta content="Preview page of Metronic Admin Theme #4 for statistics, charts, recent events and reports" name="description">
     <meta content="" name="author">
-    <!-- BEGIN GLOBAL MANDATORY STYLES -->
     <link href="{{asset('css/font-awesome.min.css')}}" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/simple-line-icons/2.4.1/css/simple-line-icons.css">
     <link href="{{asset('theme/css/bootstrap.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('theme/css/bootstrap-switch.min.css')}}" rel="stylesheet" type="text/css">
-    <!-- END GLOBAL MANDATORY STYLES -->
-    <!-- BEGIN PAGE LEVEL PLUGINS -->
     <link href="{{asset('theme/css/daterangepicker.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('theme/css/morris.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('theme/css/fullcalendar.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('theme/css/jqvmap.css')}}" rel="stylesheet" type="text/css">
-    <!-- END PAGE LEVEL PLUGINS -->
-    <!-- BEGIN THEME GLOBAL STYLES -->
     <link href="{{asset('theme/css/components.min.css')}}" rel="stylesheet" id="style_components" type="text/css">
     <link href="{{asset('theme/css/plugins.min.css')}}" rel="stylesheet" type="text/css">
-    <!-- END THEME GLOBAL STYLES -->
-    <!-- BEGIN THEME LAYOUT STYLES -->
     <link href="{{asset('theme/css/layout.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('theme/css/light.min.css')}}" rel="stylesheet" type="text/css" id="style_color">
     <link href="{{asset('theme/css/custom.min.css')}}" rel="stylesheet" type="text/css">
-    <!-- END THEME LAYOUT STYLES -->
     <link rel="shortcut icon" href="https://keenthemes.com/preview/metronic/theme/admin_4/favicon.ico">
     <style type="text/css">
         .jqstooltip {
@@ -61,46 +49,17 @@
     </style>
     @yield('css')
 </head>
-<!-- END HEAD -->
-
 <body class="page-container-bg-solid page-header-fixed page-sidebar-closed-hide-logo">
-    <!-- BEGIN HEADER -->
     @include('navbar')
-    <!-- END HEADER -->
-    <!-- BEGIN HEADER & CONTENT DIVIDER -->
     <div class="clearfix"> </div>
-    <!-- END HEADER & CONTENT DIVIDER -->
-    <!-- BEGIN CONTAINER -->
     <div class="page-container">
-        <!-- BEGIN SIDEBAR -->
         @include('menu')
-        <!-- END SIDEBAR -->
-        <!-- BEGIN CONTENT -->
         <div class="page-content-wrapper">
-            <!-- BEGIN CONTENT BODY -->
             <div class="page-content" style="min-height: 1128px;">
-
                 @yield('contenido')
-
             </div>
-            <!-- END CONTENT BODY -->
         </div>
-        <!-- END CONTENT -->
-        <!-- BEGIN QUICK SIDEBAR -->
-        <!-- END QUICK SIDEBAR -->
     </div>
-    <!-- END CONTAINER -->
-    <!-- BEGIN FOOTER -->
-    <!-- END FOOTER -->
-    <!-- BEGIN QUICK NAV -->
-
-    <!-- END QUICK NAV -->
-    <!--[if lt IE 9]>
-<script src="../assets/global/plugins/respond.min.js"></script>
-<script src="../assets/global/plugins/excanvas.min.js"></script> 
-<script src="../assets/global/plugins/ie8.fix.min.js"></script> 
-<![endif]-->
-    <!-- BEGIN CORE PLUGINS -->
     <script async="" src="{{asset('theme/js/gtm.js.download')}}"></script>
     <script async="" src="{{asset('theme/js/analytics.js.download')}}"></script>
     <script src="{{asset('theme/js/jquery.min.js.download')}}" type="text/javascript"></script>
@@ -109,8 +68,6 @@
     <script src="{{asset('theme/js/jquery.slimscroll.min.js.download')}}" type="text/javascript"></script>
     <script src="{{asset('theme/js/jquery.blockui.min.js.download')}}" type="text/javascript"></script>
     <script src="{{asset('theme/js/bootstrap-switch.min.js.download')}}" type="text/javascript"></script>
-    <!-- END CORE PLUGINS -->
-    <!-- BEGIN PAGE LEVEL PLUGINS -->
     {{--
 
         <script src="./Metronic_files/moment.min.js.download" type="text/javascript"></script>
@@ -146,19 +103,12 @@
         <script src="./Metronic_files/jquery.vmap.usa.js.download" type="text/javascript"></script>
         <script src="./Metronic_files/jquery.vmap.sampledata.js.download" type="text/javascript"></script>
     --}}
-    <!-- END PAGE LEVEL PLUGINS -->
-    <!-- BEGIN THEME GLOBAL SCRIPTS -->
     <script src="{{asset('theme/js/app.min.js.download')}}" type="text/javascript"></script>
-    <!-- END THEME GLOBAL SCRIPTS -->
-    <!-- BEGIN PAGE LEVEL SCRIPTS -->
     <script src="{{asset('theme/js/dashboard.min.js.download')}}" type="text/javascript"></script>
-    <!-- END PAGE LEVEL SCRIPTS -->
-    <!-- BEGIN THEME LAYOUT SCRIPTS -->
     <script src="{{asset('theme/js/layout.min.js.download')}}" type="text/javascript"></script>
     {{--<script src="./Metronic_files/demo.min.js.download" type="text/javascript"></script>--}}
     <script src="{{asset('theme/js/quick-sidebar.min.js.download')}}" type="text/javascript"></script>
     <script src="{{asset('theme/js/quick-nav.min.js.download')}}" type="text/javascript"></script>
-    <!-- END THEME LAYOUT SCRIPTS -->
     <script>
         $(document).ready(function () {
             $('#clickmewow').click(function () {
